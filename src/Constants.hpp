06e7e2c8c702f5008dbbbd6725211b53d725987c
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2022-11-05 18:08:36 +0300

    feat(Cons): added Constants class

---
+++ src/Constants.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+static constexpr int TILE_SIZE{64};
+
+static constexpr int MAP_NUM_ROWS{13};
+static constexpr int MAP_NUM_COLS{20};
+
+static constexpr double MINIMAP_SCALE_FACTOR{1.0};
+
+static constexpr int WINDOW_WIDTH{MAP_NUM_COLS * TILE_SIZE};
+static constexpr int WINDOW_HEIGHT{MAP_NUM_ROWS * TILE_SIZE};

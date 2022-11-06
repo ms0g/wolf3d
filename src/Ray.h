b07b8a696ae2fc122361b376b546ede3a28cd872
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2022-11-07 00:08:12 +0300

    feat(Ray): added getter methods

--- src/Ray.h
+++ src/Ray.h
@@ -20,10 +20,31 @@ public:
         bool isRayFacingRight,
         int wallHitContent);
 
+    [[nodiscard]] double Angle() const {
+        return m_angle;
+    }
+
+    [[nodiscard]] double WallHitX() const {
+        return m_wallHitX;
+    }
+
+    [[nodiscard]] double WallHitY() const {
+        return m_wallHitY;
+    }
+
+    [[nodiscard]] double Distance() const {
+        return m_distance;
+    }
+
+    [[nodiscard]] bool WasHitVertical() const {
+        return m_wasHitVertical;
+    }
+
+
     void Cast(double angle, std::unique_ptr<Player>& player, std::unique_ptr<Map>& map);
 
     void Render(SDL_Renderer* renderer, std::unique_ptr<Player>& player) const;
-
+private:
     double m_angle;
     double m_wallHitX;
     double m_wallHitY;

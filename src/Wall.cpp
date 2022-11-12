24f05016a0ea3961412996fb169ccaa22f277900
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2022-11-12 05:37:22 +0300

    refactor(Wall): changed variable name

--- src/Wall.cpp
+++ src/Wall.cpp
@@ -10,37 +10,38 @@ void Wall::Render(std::array<Ray, NUM_RAYS>& rays,
                   std::unique_ptr<Graphics>& graphics,
                   std::unique_ptr<Texture>& texture) {
     for (int x = 0; x < NUM_RAYS; ++x) {
+        // Calculate the perpendicular distance to avoid fish-eye distortion
         double perpendicularDistance = rays[x].Distance() * cos(rays[x].Angle() - player->rotationAngle);
-        double projectedWallHeight = (TILE_SIZE / perpendicularDistance) * DIST_PROJ_PLANE;
 
-        int wallStripHeight = static_cast<int>(projectedWallHeight);
+        // Calculate the projected wall height
+        double wallHeight = (TILE_SIZE / perpendicularDistance) * DIST_PROJ_PLANE;
 
-        int wallTopPixel = (WINDOW_HEIGHT / 2) - (wallStripHeight / 2);
-        wallTopPixel = wallTopPixel < 0 ? 0 : wallTopPixel;
+        int wallTopY = (WINDOW_HEIGHT / 2) - (wallHeight / 2);
+        wallTopY = wallTopY < 0 ? 0 : wallTopY;
 
-        int wallBottomPixel = (WINDOW_HEIGHT / 2) + (wallStripHeight / 2);
-        wallBottomPixel = wallBottomPixel > WINDOW_HEIGHT ? WINDOW_HEIGHT : wallBottomPixel;
+        int wallBottomY = (WINDOW_HEIGHT / 2) + (wallHeight / 2);
+        wallBottomY = wallBottomY > WINDOW_HEIGHT ? WINDOW_HEIGHT : wallBottomY;
 
         // color of the ceiling
-        for (int y = 0; y < wallTopPixel; ++y) {
+        for (int y = 0; y < wallTopY; ++y) {
             graphics->DrawPixel(x, y, 0xFF333333);
         }
 
         // get the texture id from map content
         int texNum = rays[x].WallHitContent() - 1;
 
-        int textureWidht = texture->GetTexture(texNum).width;
-        int textureHeight = texture->GetTexture(texNum).height;
+        int textureWidht = upng_get_width(texture->GetTexture(texNum));
+        int textureHeight = upng_get_height(texture->GetTexture(texNum));
 
         // calculate texture offset X
-        int textureOffsetX = rays[x].WasHitVertical() ? static_cast<int>(rays[x].WallHitY()) % textureHeight :
+        auto textureOffsetX = rays[x].WasHitVertical() ? static_cast<int>(rays[x].WallHitY()) % textureHeight :
                              static_cast<int>(rays[x].WallHitX()) % textureWidht;
 
         // color of the wall
-        for (int y = wallTopPixel; y < wallBottomPixel; ++y) {
+        for (int y = wallTopY; y < wallBottomY; ++y) {
             // calculate texture offset Y
-            int distanceFromTop = y + (wallStripHeight / 2) - (WINDOW_HEIGHT / 2);
-            int textureOffsetY = distanceFromTop * (static_cast<float>(textureHeight) / wallStripHeight);
+            int distanceFromTop = y + (wallHeight / 2) - (WINDOW_HEIGHT / 2);
+            int textureOffsetY = distanceFromTop * (static_cast<float>(textureHeight) / wallHeight);
 
             // set the color of the wall based on the color from the texture
             color_t texelColor = texture->GetColor(textureOffsetX, textureOffsetY, texNum);
@@ -53,7 +54,7 @@ void Wall::Render(std::array<Ray, NUM_RAYS>& rays,
         }
 
         // color of the floor
-        for (int y = wallBottomPixel; y < WINDOW_HEIGHT; ++y) {
+        for (int y = wallBottomY; y < WINDOW_HEIGHT; ++y) {
             graphics->DrawPixel(x, y, 0xFF777777);
         }
     }

6259f63447cda3901bd4484c29cec0876aac4da0
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2022-11-12 03:21:25 +0300

    feat(Utils): added change color intensity function

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -12,3 +12,12 @@ void normalizeAngle(double& angle) {
 double distanceBetweenPoints(double x1, double y1, double x2, double y2) {
     return sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
 }
+
+void changeColorIntensity(uint32_t& color, float factor) {
+    uint32_t a = color & 0xFF000000;
+    uint32_t r = (color & 0x00FF0000) * factor;
+    uint32_t g = (color & 0x0000FF00) * factor;
+    uint32_t b = (color & 0x000000FF) * factor;
+
+    color = a | (r & 0x00FF0000) | (g & 0x0000FF00) | (b & 0x000000FF);
+}
--- src/Utils.h
+++ src/Utils.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <cstdint>
+
 void normalizeAngle(double& angle);
 
 double distanceBetweenPoints(double x1, double y1, double x2, double y2);
+
+void changeColorIntensity(uint32_t& color, float factor);

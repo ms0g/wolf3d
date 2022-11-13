ccd31f9b226ef732a00cbd319c0c56be051ac53b
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2022-11-13 03:43:11 +0300

    refactor(Constants): changed double to float

--- src/Constants.hpp
+++ src/Constants.hpp
@@ -11,17 +11,15 @@ constexpr int TILE_SIZE{64};
 constexpr int MAP_NUM_ROWS{13};
 constexpr int MAP_NUM_COLS{20};
 
-constexpr double MINIMAP_SCALE_FACTOR{0.2};
+constexpr float MINIMAP_SCALE_FACTOR{0.2};
 
 constexpr int WINDOW_WIDTH{1280};
 constexpr int WINDOW_HEIGHT{800};
 
-constexpr int NUM_TEXTURES{8};
-
 constexpr int NUM_RAYS{WINDOW_WIDTH};
 
-constexpr double FOV_ANGLE{(60 * (std::numbers::pi / 180))};
+constexpr float FOV_ANGLE{(60 * (std::numbers::pi / 180))};
 
-static const double DIST_PROJ_PLANE{(WINDOW_WIDTH / 2) / tan(FOV_ANGLE / 2)};
+static const float DIST_PROJ_PLANE{static_cast<float>((WINDOW_WIDTH / 2) / tan(FOV_ANGLE / 2))};
 
 using color_t = uint32_t;
